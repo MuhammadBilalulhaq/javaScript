25176e03f3c11aa0f27abcc0e4afc0ad58a366e9
Muhammad Bilal Ul Haq <bulhaq60@gmail.com>
2026-08-28 13:01:09 +0500

    Add files via upload

---
+++ java-script2/js/index.js
@@ -0,0 +1,18 @@
+
+
+
+// Loops
+function greetUser(user){
+    alert("Good Morning"+ " "+ user);
+}
+// function mouseover(){
+// alert();
+// }
+function mouseover (image){
+console.log(image);
+image.src='./assets/card-front.jpg'
+}
+function mouseOut(image){
+    console.log(image);
+    image.src='./assets/card-back.jpg'
+}
